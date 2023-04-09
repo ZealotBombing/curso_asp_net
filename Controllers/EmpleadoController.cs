d2846d8bf8adc86d0bf0eab0e851690f860494bd
ZealotBombing <cfuentesguerrero333@gmail.com>
2023-04-09 01:22:28 -0400

    llenar multiples combobox

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -33,5 +33,64 @@ namespace Pasaje5.Controllers
             }
                 return View(listaEmpleado);//recuerda mandar el model con los datops
         }
+        public void listarComboSexo()
+        {
+            List<SelectListItem> lista;
+            using(var bd = new BDPasajeEntities1())
+            {
+                lista = (from sexo in bd.Sexo
+                         where sexo.BHABILITADO == 1
+                         select new SelectListItem
+                         {
+                             Text = sexo.NOMBRE,
+                             Value = sexo.IIDSEXO.ToString()
+                         }).ToList();
+                lista.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listaSexo = lista;
+            }
+        }
+        public void listarTipoContrato()
+        {
+            List<SelectListItem> lista;
+            using (var bd = new BDPasajeEntities1())
+            {
+                lista = (from tipoContrato in bd.TipoContrato
+                         where tipoContrato.BHABILITADO == 1
+                         select new SelectListItem
+                         {
+                             Text = tipoContrato.NOMBRE,
+                             Value = tipoContrato.IIDTIPOCONTRATO.ToString()
+                         }).ToList();
+                lista.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listaTipoContrato = lista;
+            }
+        }
+        public void listarTipoUsuario()
+        {
+            List<SelectListItem> lista;
+            using (var bd = new BDPasajeEntities1())
+            {
+                lista = (from tipoUsuario in bd.TipoUsuario
+                         where tipoUsuario.BHABILITADO == 1
+                         select new SelectListItem
+                         {
+                             Text = tipoUsuario.NOMBRE,
+                             Value = tipoUsuario.IIDTIPOUSUARIO.ToString()
+                         }).ToList();
+                lista.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listaTipoUsuario = lista;
+            }
+        }
+        public void listarCombos()
+        {
+            listarComboSexo();
+            listarTipoContrato();
+            listarTipoUsuario();
+        }
+        public ActionResult Agregar()
+        {
+            listarCombos();
+            return View();
+        }
     }
 }
